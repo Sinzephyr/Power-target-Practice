11534565a126a44a08a4cf853e8a4b36d86a56c3
Sinzephyr <student@c515-d25hq0nldhjt-0072012.local>
2017-05-17 17:55:59 -0400

    Frost target practice

---
+++ Assets/Script/Laser.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+	public GameObject prefab;
+	public float laserPower;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray beam = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		Debug.DrawRay (beam.origin, beam.direction * 1000f, Color.red);
+
+		RaycastHit beamHit = new RaycastHit ();
+		if (Physics.Raycast (beam, out beamHit, 1000f)) {
+			if (beamHit.rigidbody && Input.GetMouseButton (2)) {
+				beamHit.rigidbody.AddForce (Random.insideUnitSphere * laserPower);
+			
+			}
+			if (Input.GetMouseButton (0)) {
+				Instantiate (prefab, beamHit.point, Quaternion.identity);
+			}
+
+		}
+	}
+}
